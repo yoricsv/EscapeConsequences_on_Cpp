a63497f2ebc8ff7dfcb7bd541893f8397386e74e
Сергей <yoric@live.ru>
2020-11-22 19:15:23 +0300

    Change spaces

--- Escape_consequences/main.cpp
+++ Escape_consequences/main.cpp
@@ -35,39 +35,39 @@ int main(void)
 */
 
     // FORMATTING OUTPUT IN CONSOLE BY ESCAPE CONSEQUENCES BEGIN //
-	std::cout	<<	"\n\n ^- This use two escape consequences of\t-\\n (End Line).	";
-	cout		<<	"\n\t|\t <- There is use two tabulations divided by line[|].	\n" << endl;
+	std::cout << "\n \n      ^  This use two escape consequences of \t - \\n (End Line). ";
+	     cout << "\n \t | \t <- There is use two tabulations divided by line[|].	\n   " << endl;
 
 	// SET RIGHT LOCALIZATION BEGIN //
 	setlocale(LC_ALL, "Ru-RU");
 	// SET RIGHT LOCALIZATION END //
 
-	cout		<<	"\t\t Текст \"В лесу родилась елочка\"	" << endl;
-	cout		<<	"\t\t (Автор слов Р.Кудашева)			\n\n";
-	cout		<<	"\t\t В лесу родилась елочка,			" << endl;
-	cout		<<	"\t\t В лесу она росла,					" << endl;
-	cout		<<	"\t\t Зимой и летом стройная,			" << endl;
-	cout		<<	"\t\t Зеленая была.						\n" << endl;
-	cout		<<	"\t\t Метель ей пела песенку :			" << endl;
-	cout		<<	"\t\t \"Спи, елочка, бай-бай!\"			" << endl;
-	cout		<<	"\t\t Мороз снежком укутывал :			" << endl;
-	cout		<<	"\t\t \"Смотри, не замерзай!\"			\n" << endl;
-	std::cout	<<	"\t\t Трусишка - зайка серенький		" << std::endl;
-	std::cout	<<	"\t\t Под елочкой скакал,				" << std::endl;
-	std::cout	<<	"\t\t Порою волк, сердитый волк,		" << std::endl;
-	std::cout	<<	"\t\t Рысцою пробегал.					\n" << std::endl;
-	std::cout	<<	"\t\t Чу!Снег по лесу частому			" << std::endl;
-	std::cout	<<	"\t\t Под полозом скрипит;				" << std::endl;
-	std::cout	<<	"\t\t Лошадка мохноногая				" << std::endl;
-	std::cout	<<	"\t\t Торопится, бежит.					\n" << std::endl;
-	cout		<<	"\t\t Везет лошадка дровенки,			\n";
-	cout		<<	"\t\t А в дровнях - мужичок,			\n";
-	cout		<<	"\t\t Срубил он нашу елочку				\n";
-	cout		<<	"\t\t Под самый корешок.				\n\n";
-	std::cout	<<	"\t\t Теперь она, нарядная,				\n";
-	std::cout	<<	"\t\t На праздник к нам пришла			\n";
-	std::cout	<<	"\t\t И много, много радостей			\n";
-	std::cout	<<	"\t\t Детишкам принесла.				\n";
+	     cout << "\t\t Текст \"В лесу родилась елочка\"     " <<      endl;
+	     cout << "\t\t (Автор слов Р.Кудашева)          \n\n";
+	     cout << "\t\t В лесу родилась елочка,              " <<      endl;
+	     cout << "\t\t В лесу она росла,                    " <<      endl;
+	     cout << "\t\t Зимой и летом стройная,              " <<      endl;
+	     cout << "\t\t Зеленая была.                      \n" <<      endl;
+	     cout << "\t\t Метель ей пела песенку :             " <<      endl;
+	     cout << "\t\t \"Спи, елочка, бай-бай!\"            " <<      endl;
+	     cout << "\t\t Мороз снежком укутывал :             " <<      endl;
+	     cout << "\t\t \"Смотри, не замерзай!\"           \n" <<      endl;
+	std::cout << "\t\t Трусишка - зайка серенький           " << std::endl;
+	std::cout << "\t\t Под елочкой скакал,                  " << std::endl;
+	std::cout << "\t\t Порою волк, сердитый волк,           " << std::endl;
+	std::cout << "\t\t Рысцою пробегал.                   \n" << std::endl;
+	std::cout << "\t\t Чу!Снег по лесу частому              " << std::endl;
+	std::cout << "\t\t Под полозом скрипит;                 " << std::endl;
+	std::cout << "\t\t Лошадка мохноногая                   " << std::endl;
+	std::cout << "\t\t Торопится, бежит.                  \n" << std::endl;
+	     cout << "\t\t Везет лошадка дровенки,            \n";
+	     cout << "\t\t А в дровнях - мужичок,             \n";
+	     cout << "\t\t Срубил он нашу елочку              \n";
+	     cout << "\t\t Под самый корешок.               \n\n";
+	std::cout << "\t\t Теперь она, нарядная,              \n";
+	std::cout << "\t\t На праздник к нам пришла           \n";
+	std::cout << "\t\t И много, много радостей            \n";
+	std::cout << "\t\t Детишкам принесла.                 \n";
 	// FORMATTING OUTPUT IN CONSOLE BY ESCAPE CONSEQUENCES END //
 
 	// CODE/INSTRUCTIONS END //
